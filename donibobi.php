421899b80725ce4bca4518c460591e36e1bd69db
praisleynangkok <praisleyn321@gmail.com>
2024-06-14 23:08:58 +0700

    Add files via upload

---
+++ donibobi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class donibobi extends Model
+{
+    protected $table ="donibobi";
+
+    protected $fillable = ['id','item','jumlah'];
+}
